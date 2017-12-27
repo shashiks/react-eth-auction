6b8510620722d6005fc6339de2e6b838217d73a1
shashiks <soni.shashikant@gmail.com>
2017-12-27 16:50:28 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+
+This is a demo project with samples for :
+<ul>
+<li>Ethereum Smart Contract</li>
+<li>IPFS</li>
+</ul>
+
+<p>It uses ReactJS as the frontend framework, bootstrap for layouts, webpack for packaging.<p>
+<p>Truffle is used for communicating with ETH and deploying code to blockchain</p>
+
+There are lots of optimizations pending. Particularaly:
+<ul>
+<li>React component isolation between view and data</li>
+<li>Bootstrap view updates in place instead of full page refresh</li>
+</ul>
+But for now the code is more of a sample to work with ETH and IPS. The front end optimizations
+would happen in future.
+
+Feel free to use this as a template to get started on these technologies.
+
+
+Bootstrap theme template is from https://startbootstrap.com/template-overviews/sb-admin/
+This project was created using create-react-app https://github.com/facebookincubator/create-react-app
---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+
+class App extends Component {
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      feature: 'Auction',
+      sub_feature: 'create',
+      message: "Welcome to "
+    }
+    this.statusUpdate = this.statusUpdate.bind(this);
+        
+  }
+
+
+  statusUpdate() {
+    var curStatus = this.state.feature;
+    if(curStatus == 'Auction') {
+      curStatus = 'File Mgmt';
+      this.setState({sub_feature: 'Files'});
+    } else {
+      curStatus = "Auction";
+      this.setState({sub_feature: 'Create'});
+    }
+
+    this.setState({feature: curStatus});
+  }
+
+  render() {
+    return (
+      <div>
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item active">{this.state.feature}</li>
+          <li class="breadcrumb-item">{this.state.sub_feature}</li>
+        </ol>
+         <div class="card mb-3">
+          <div class="card-header"></div>
+          <div class="card-body">
+            {this.state.message} {this.state.feature}
+            <p><input type="button" value="Change" id="changeState" onClick={this.statusUpdate}/></p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
